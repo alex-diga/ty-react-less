61bef4e754faa25b77081620930282f15ee875fe
garder77 <2635204377@qq.com>
2020-03-11 17:48:50 +0800

    使用Ma_Shan_Zheng字体

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './font/Ma_Shan_Zheng/MaShanZheng-Regular.ttf'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
---
+++ src/page/home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.less';
+
+class Home extends React.Component {
+    render() {
+    	return (
+    		<div className="homePage">
+    			首页
+    			<span className="tip">这是首页</span>
+    		</div>
+    	)
+    }
+}
+
+export default Home
