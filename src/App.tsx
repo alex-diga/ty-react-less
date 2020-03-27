3dd15fef3c39ab8e9ba54ac1c1426ac0e385befa
garder77 <2635204377@qq.com>
2020-03-27 09:27:04 +0800

    解决react-router-dom的问题

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import './App.less';
 import  Home from './page/home'
-
 
 function App() {
   return (
     <div className="App">
-      <Home />
+    	<BrowserRouter>
+    		<Switch>
+    			<Route path="/index" component={Home} />
+    			<Redirect from="/" exact to="/index" />
+    		</Switch>
+    	</BrowserRouter>
     </div>
   );
 }
